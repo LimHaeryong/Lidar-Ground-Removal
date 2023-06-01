ab6bd2a11d6a3105e4a57670f8e6f0ec81a8dd10
LimHaeryong <nabi4622@snu.ac.kr>
2023-06-02 01:21:04 +0900

    lidar setting

--- Dependency.cmake
+++ Dependency.cmake
@@ -1,3 +1,28 @@
+include(ExternalProject)
+
+set(DEP_INSTALL_DIR ${PROJECT_BINARY_DIR}/install)
+set(DEP_INCLUDE_DIR ${DEP_INSTALL_DIR}/include)
+set(DEP_LIB_DIR ${DEP_INSTALL_DIR}/lib)
+
+# spdlog
+ExternalProject_Add(
+    dep-spdlog
+    GIT_REPOSITORY "https://github.com/gabime/spdlog.git"
+    GIT_TAG "v1.x"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ""
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+    TEST_COMMAND ""
+)
+
+set(DEP_LIST ${DEP_LIST} dep-spdlog)
+if (MSVC)
+  set(DEP_LIBS ${DEP_LIBS} spdlog$<$<CONFIG:Debug>:d>)
+else()
+  set(DEP_LIBS ${DEP_LIBS} spdlog)
+endif()
+
 set(libCarla_INSTALL_DIR ${CMAKE_SOURCE_DIR}/libcarla-install)
 set(libCarla_INCLUDE_DIR ${libCarla_INSTALL_DIR}/include)
 set(libCarla_LIB_DIR ${libCarla_INSTALL_DIR}/lib)
@@ -16,7 +41,9 @@ set(libCarla_LIBS
     ${libCarla_LIB_DIR}/libboost_program_options.so
     ${libCarla_LIB_DIR}/libboost_system.so
 )
-
-set(DEP_INCLUDE_DIR ${libCarla_INCLUDE_DIR}/system)
+set(libCarla_INCLUDE_DIR ${libCarla_INCLUDE_DIR} ${libCarla_INCLUDE_DIR}/system)
 
 find_package(PCL REQUIRED)
+add_definitions(${PCL_DEFINITIONS})
+
+find_package(yaml-cpp REQUIRED)
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,93 @@
 #include <carla/client/Client.h>
+#include <carla/client/BlueprintLibrary.h>
+
+#include <yaml-cpp/yaml.h>
+#include <spdlog/spdlog.h>
 
 #include <chrono>
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
 
 using namespace std::chrono_literals;
 
-int main(int argc, char** argv)
+int main()
 {
-	auto client = carla::client::Client("localhost", 2000);
-	client.SetTimeout(2s);
-	auto world = client.GetWorld();
-	client.LoadWorld("Town01");
+	SPDLOG_INFO("Start program");
 
+	YAML::Node config = YAML::LoadFile("./config/config.yaml");
+	if(!config.IsDefined())
+	{
+		SPDLOG_INFO("Config load failed.");
+		return 1;
+	}
+
+	const auto HOST = config["CLIENT"]["HOST"].as<std::string>();
+	const auto PORT = config["CLIENT"]["PORT"].as<uint16_t>();
+	const auto MAP = config["MAP"].as<std::string>();
+	const auto VEHICLE_ID = config["VEHICLE_ID"].as<std::string>();
+	const auto LIDAR = config["SENSOR"]["LIDAR"];
+	const auto LIDAR_LOCATION = LIDAR["LOCATION"].as<std::vector<float>>();
+	const auto LIDAR_ROTATION = LIDAR["ROTATION"].as<std::vector<float>>();
+
+	SPDLOG_INFO("Initialize client");
+	auto client = carla::client::Client(HOST, PORT);
+	client.SetTimeout(2s);
+	SPDLOG_INFO("Load world {}", MAP);
+	client.LoadWorld(MAP);
+
+	auto world = client.GetWorld();
+	auto bluePrintLibrary = world.GetBlueprintLibrary();
+	auto vehicleLibrary = bluePrintLibrary->Filter("vehicle");
+	auto vehicleModel = *(vehicleLibrary->Find(VEHICLE_ID));
+	
+	auto map = world.GetMap();
+	auto spawnPoint = map->GetRecommendedSpawnPoints().front();
+	auto vehicleActor = world.SpawnActor(vehicleModel, spawnPoint);
+	auto vehicle = boost::static_pointer_cast<carla::client::Vehicle>(vehicleActor);
+	
+	auto spectator = world.GetSpectator();
+	auto specTransform = spawnPoint;
+	specTransform.location += 32.0f * specTransform.GetForwardVector();
+    specTransform.location.z += 2.0f;
+    specTransform.rotation.yaw += 180.0f;
+    specTransform.rotation.pitch = -15.0f;
+    spectator->SetTransform(specTransform);
+
+	auto lidarModel = *(bluePrintLibrary->Find(LIDAR["MODEL"].as<std::string>()));
+	lidarModel.SetAttribute("upper_fov", LIDAR["UPPER_FOV"].as<std::string>());
+	lidarModel.SetAttribute("lower_fov", LIDAR["LOWER_FOV"].as<std::string>());
+	lidarModel.SetAttribute("channels", LIDAR["CHANNELS"].as<std::string>());
+	lidarModel.SetAttribute("range", LIDAR["RANGE"].as<std::string>());
+	lidarModel.SetAttribute("rotation_frequency", LIDAR["ROTATION_FREQUENCY"].as<std::string>());
+	lidarModel.SetAttribute("points_per_second", LIDAR["POINTS_PER_SECOND"].as<std::string>());
+
+	auto lidarLocation = carla::geom::Location(LIDAR_LOCATION[0], LIDAR_LOCATION[1], LIDAR_LOCATION[2]);
+	auto lidarRotation = carla::geom::Rotation(LIDAR_ROTATION[0], LIDAR_ROTATION[1], LIDAR_ROTATION[2]);
+	auto lidarTransform = carla::geom::Transform(lidarLocation, lidarRotation);
+	auto lidarActor = world.SpawnActor(lidarModel, lidarTransform, vehicleActor.get());
+
+	bool exitLoop = false;
+	SPDLOG_INFO("Start main loop");
+	while(!exitLoop)
+	{
+		if(std::cin.peek() != EOF)
+		{
+			char key;
+			std::cin >> key;
+			if(key == 27)
+			{
+				exitLoop = true;
+			}
+		}
+
+		SPDLOG_INFO("loop");
+		std::this_thread::sleep_for(1s);
+	}
+
+	vehicle->Destroy();
+	SPDLOG_INFO("End program");
 	return 0;
 }
